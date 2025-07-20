5986cf3ad65b5c98ffa0ee447970e024a3261346
Shivam9070 <shivamrajput945083@gmail.com>
2025-07-20 18:51:07 +0530

    added authrisation {very imp for future apw}

---
+++ 12MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,65 @@
+import Config from '../Config'
+import {Client ,Account ,ID} from "appwrite";
+
+
+
+export class AuthService {
+    client =new Client ();
+    account;
+
+
+    constructor(){
+        this.client
+            .setEndpoint(Config.appwriteURl)
+            .setProject(appwriteProjectId);
+        this.account =new Account(this.client);
+
+        
+    }
+
+    async createAccount({email,password,name}){
+        try{
+            const userAccount =await this.account.create(ID.unique ,email,password)
+
+            if(userAccount){
+                //loogni
+                return this.login({email,password});
+            }else{
+                return userAccount;
+            }
+        }
+        catch(error){
+            throw error;
+        }
+    }
+    async login({email,password}){
+        try {
+            return await this.account.createEmailPasswordSession(email,password,)
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite service ::getr cureent user :: eror",error);
+            
+        }
+        return null;
+    }
+
+    async logout(){
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite service ::getr cureent user :: eror",error);
+            
+        }
+    }
+}
+
+const authService =new  AuthService();
+
+export default authService
