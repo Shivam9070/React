2af7d5a92e491355d96dacaafea2df33f5bdd6b0
Shivam9070 <shivamrajput945083@gmail.com>
2025-07-20 18:20:55 +0530

    added environment and appwrite backend for this project

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>React blog App</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 12MegaBlog/src/Config/Config.js
@@ -0,0 +1,10 @@
+const Config={
+    appwriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId:String(import.meta.env.VITE_PROJECT_ID),
+    appwriteDatabaseId:String(import.meta.env.VITE_DATABASE_ID),
+    appwriteCllectionId:String(import.meta.env.VITE_COLLECTION_ID),
+    appwriteBucketId:String(import.meta.env.VITE_BUCKET_ID),
+}
+
+
+export default Config
