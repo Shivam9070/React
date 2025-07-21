7f8f6667dd5d08ab72d74f7b446d88688ca06317
Shivam9070 <shivamrajput945083@gmail.com>
2025-07-21 14:57:11 +0530

    toolkit

--- 12MegaBlog/src/appwrite/config.js
+++ 12MegaBlog/src/appwrite/config.js
@@ -1,141 +1,137 @@
-import Config from "../Config"
-import {Client ,ID,Database,Storage ,Query} from "aapwrite";
+import conf from '../conf/conf.js'
+import { Client, ID, Databases, Storage, Query } from "appwrite";
 
-export class Service {
-    client =new Client();
+export class Service{
+    client = new Client();
     databases;
     bucket;
+    
     constructor(){
-            this.client
-                .setEndpoint(Config.appwriteURl)
-                .setProject(appwriteProjectId);
-            this.databases =new Database(this.client);
-            this,bucket=new Storage(this.client);
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+
+    async createPost({title, slug, content, featuredImage, status, userId}){
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite serive :: createPost :: error", error);
+        }
+    }
+
+    async updatePost(slug, {title, content, featuredImage, status}){
+        try {
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite serive :: updatePost :: error", error);
+        }
+    }
+
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
             
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite serive :: deletePost :: error", error);
+            return false
         }
+    }
 
-        async createPost({title,slug,content,featuredimage,status,userid}){
-            try {
-                return await this.databases.createDocument(
-                    Config.appwriteDatabaseId,
-                    Config.appwriteCollectionId,
-                    slug,
-                    //document id alt
-                    {
-                        title,
-                        content,
-                        featuredimage,
-                        status,
-                        userid
-                    }
-                )
-                
-            } catch (error) {
-                console.log("Appwrite service ::createPost ::error",error);
-            }
-
-        }
-        async updatePost({title,slug,content,featuredimage,status,userid}){
-            try {
-                return await this.databases.updateDocument(
-                    Config.appwriteDatabaseId,
-                    Config.appwriteCollectionId,
-                    slug,
-                    {
-                        title,
-                        content,
-                        featuredimage,
-                        status
-
-                    }
-                )
-            } catch (error) {
-                console.log("appwrite service ::createPost ::error",error);
-                
-            }
-        }
-
-        async deletePost(slug){
-            try {
-                await this.databases.deleteDocument(
-                    Config.appwriteDatabaseId,
-                    Config.appwriteCollectionId,
-                    slug
-                )
-                return true;
-            } catch (error) {
-                console.log("appwrite service ::createPost ::error",error);
-                return false;
-            }
-        }
-
-        async getPost(slug){
-            try {
-                return await this.databases.getDocument(
-                    Config.appwriteDatabaseId,
-                    Config.appwriteCollectionId,
-                    slug
-                )
-            } catch (error) {
-                console.log("Appwrite service ::createPost ::error",error);
-            }
-
-        }
-//Active post get 
-        async getPosts(queries =[Query.equal("status","active")]){
-            try {
-                return await this.databases.listDocuments(
-                    Config.appwriteDatabaseId,
-                    Config.appwriteCollectionId,
-                    queries,
-
-
-                )
-            } catch (error) {
-                console.log("Appwrite service ::createPost ::error",error);
-                return false;
-            }
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
             
+            )
+        } catch (error) {
+            console.log("Appwrite serive :: getPost :: error", error);
+            return false
         }
+    }
 
-        //file upload Service 
-        async uploadFile(file){
-            try {
-                return await this.bucket.createFile(
-                    Config.appwriteBucketId,
-                    ID.unique(),
-                    file
-                )
+    async getPosts(queries = [Query.equal("status", "active")]){
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries,
                 
-            } catch (error) {
-                console.log("Appwrite service ::createPost ::error",error);
-                return false;
-            }
-        }
 
-        async deleteFile(fileId){
-            try {
-                await this.bucket.deleteFile(
-                    Config.appwriteBucketId,
-                    fileId
-                )
-                return true;
-            } catch (error) {
-                console.log("Appwrite service ::createPost ::error",error);
-                return false;
-            }
-
+            )
+        } catch (error) {
+            console.log("Appwrite serive :: getPosts :: error", error);
+            return false
         }
+    }
 
-        getFilePreview(fileId){
-            this.bucket.deleteFile(
-                    Config.appwriteBucketId,
-                    fileId
-                )
+    // file upload service
+
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite serive :: uploadFile :: error", error);
+            return false
         }
+    }
+
+    async deleteFile(fileId){
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite serive :: deleteFile :: error", error);
+            return false
+        }
+    }
+
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
+    }
 }
 
 
-
-const service =new service
-export default Service
+const service = new Service()
+export default service
--- 12MegaBlog/src/conf/conf.js
+++ 12MegaBlog/src/conf/conf.js
@@ -1,4 +1,4 @@
-const Config={
+const conf={
     appwriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
     appwriteProjectId:String(import.meta.env.VITE_PROJECT_ID),
     appwriteDatabaseId:String(import.meta.env.VITE_DATABASE_ID),
@@ -7,4 +7,4 @@ const Config={
 }
 
 
-export default Config
+export default conf
